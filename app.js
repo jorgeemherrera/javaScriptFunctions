3be2186398c8458396ad25d801b35a9d412b00a6
Jorge Eliecer <jorge.m.h@outlook.com>
2020-04-26 14:31:52 -0500

    introducing functions

---
+++ app.js
@@ -0,0 +1,14 @@
+function greeting() {
+  return 'Hello Jorge';
+}
+
+let message = greeting();
+console.log('message: ', message);
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+let result = sum(2, 4);
+
+console.log('sum ' , result)
